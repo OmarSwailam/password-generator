11d38cdbf160b9602dc753de10a8de2c5c5a9da6
Omar Swailam <omarswailam@outlook.com>
2025-02-23 22:54:43 +0200

    feat: generate and copy functionalities

--- index.js
+++ index.js
@@ -91,3 +91,48 @@ const characters = [
   "?",
   "/",
 ];
+
+password1El = document.getElementById("password-1");
+password2El = document.getElementById("password-2");
+
+function generate() {
+  let password1 = "";
+  let password2 = "";
+  for (let i = 0; i < 15; i++) {
+    let randomNumber = Math.floor(Math.random() * 91);
+    password1 += characters[randomNumber];
+  }
+  for (let i = 0; i < 15; i++) {
+    let randomNumber = Math.floor(Math.random() * 91);
+    password2 += characters[randomNumber];
+  }
+  password1El.innerText = password1;
+  password2El.innerText = password2;
+}
+
+function copy(event) {
+  const textToCopy = event.target.innerText;
+  // the clipboard api not working unless it's a secure/https connection
+  // navigator.clipboard.writeText(textToCopy);
+  // this method creates a text area and copy the text from it and then removes it
+  const textArea = document.createElement("textarea");
+  textArea.value = textToCopy;
+  // make it hidden
+  textArea.style.opacity = "0";
+  textArea.style.position = "fixed";
+  // adding the text area to the html doc
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  document.execCommand("copy");
+  // removing the text area from the html doc
+  document.body.removeChild(textArea);
+
+  event.target.style.cursor = 'url("./images/copied.png"), auto';
+  event.target.addEventListener("mouseleave", () => {
+    event.target.style.cursor = 'url("./images/copy.png"), auto';
+  });
+}
+
+password1El.addEventListener("click", copy);
+password2El.addEventListener("click", copy);
